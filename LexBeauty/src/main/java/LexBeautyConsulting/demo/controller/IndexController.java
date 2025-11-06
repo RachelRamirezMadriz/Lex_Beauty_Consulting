5db8809ee023a9ad807e21e3030860dfd8252ae2
Jared <jchacon00466@ufide.ac.cr>
2025-11-06 14:14:19 -0600

    cambios controller index
    creado el indexcontroller

--- LexBeauty/src/main/java/LexBeautyConsulting/demo/controller/IndexController.java
+++ LexBeauty/src/main/java/LexBeautyConsulting/demo/controller/IndexController.java
@@ -1,13 +1,29 @@
 package LexBeautyConsulting.demo.controller;
 
+import LexBeautyConsulting.demo.services.CategoriaService;
+import LexBeautyConsulting.demo.services.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @Autowired
+    private ProductoService productoService;
+
     @GetMapping({"/", "/index"})
-    public String index() {
+    public String index(Model model) {
+        var categorias = categoriaService.getCategorias(true);
+        var productos = productoService.getProductos(true);
+
+        model.addAttribute("productos", productos);
+        model.addAttribute("categorias", categorias);
+        // CARGAR CATEGORIAS Y PRODUCTOS EN EL INDEX
         return "index";
     }
 
